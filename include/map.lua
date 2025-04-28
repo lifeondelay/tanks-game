89d2937d2d7c4ca3eded182b15240ba4039f7e23
lifeondelay <nickcasey1995@gmail.com>
2025-04-28 20:39:00 +0930

    fixed map rendering

--- include/map.lua
+++ include/map.lua
@@ -2,7 +2,8 @@ local Object = require("include.classic")
 
 local Map = Object:extend()
 
-local screenX, screenY = love.window.getDesktopDimensions()
+local screenX, screenY = love.window.getMode()
+
 
 -- Creates a default map
 function Map:default_map()
@@ -33,6 +34,7 @@ end
 function Map:new(width, height, walls)
     self.width = width or 16
     self.height = height or 16
+    self.block_size = (screenX / self.width) or 50
 
     self.walls = walls or self.default_map(self)
     self.real_map = self.map(self)
@@ -40,40 +42,42 @@ end
 
 -- Creates a table of rectangles based on the 2d array for the map
 function Map:map()
-    local block_size = screenX / self.width
-
     local real_map = {}
     for i = 1, self.width do
         for j = 1, self.height do
             if self.walls[i][j] == 1 then
                 local rect = {}
-                rect.x = block_size * (j - 1)
-                rect.y = block_size * (i - 1)
+                rect.x = self.block_size * (j - 1)
+                rect.y = self.block_size * (i - 1)
                 table.insert(real_map, rect)
             end
         end
     end
+
+    for i = 1, #real_map do
+        print(real_map[i].x .. ", " .. real_map[i].y)
+    end
+
     return real_map
 end
 
-function Map:isColliding(x, y)
-    for i = 1, #self:map() do
-        if x >= self:map()[i].x and x >= self:map()[i].x and y >= self:map()[i].y and y >= self:map()[i].y then
+function Map:isColliding(x, y, size)
+    for i = 1, #self.real_map do
+        if (x + size > self.real_map[i].x)
+        and (x < self.real_map[i].x)
+        and (y >= self.real_map[i].y)
+        and (y >= self.real_map[i].y) then
             print("colliding")
         end
     end
 end
 
 function Map:draw()
-
-    local block_size = screenX / self.width
-
     for i = 1, #self.real_map do
         love.graphics.setColor(0, 0, 0)
-                love.graphics.rectangle(
-                "line", self.real_map[i].x, self.real_map[i].y, block_size, block_size
-            )
-                love.graphics.setColor(0, 0, 0)
+        love.graphics.rectangle("line", self.real_map[i].x, self.real_map[i].y, self.block_size, self.block_size)
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.print(self.real_map[i].x .. ", " .. self.real_map[i].y, self.real_map[i].x, self.real_map[i].y)
     end
 end
 
