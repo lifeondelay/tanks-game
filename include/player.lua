0c01670c9d8695b27f412e7a977069e3d1b7811e
lifeondelay <nickcasey1995@gmail.com>
2025-05-24 17:18:50 +0930

    updated player to use physics engine

--- include/player.lua
+++ include/player.lua
@@ -2,35 +2,53 @@ local Actor = require("include.actor")
 
 local Player = Actor:extend()
 
-function Player:new(x, y, radius)
-    Player.super.new(self, x, y)
+function Player:new(world, x, y, radius)
+    Player.super.new(self, world, x, y)
     self.radius = radius or 10
+    self.body = love.physics.newBody(world, x, y, "dynamic")
     self.isColliding = false
+    self.shape = love.physics.newCircleShape(self.radius)
+    self.fixture = love.physics.newFixture(
+        self.body, self.shape, 5
+    )
+    self.fixture:setRestitution(0.9)
+
+    self.x = self.body:getX()
+    self.y = self.body:getY()
+
+    print(self.body:getX())
+    print(self.body:getY())
+    print(self.shape:getRadius())
 end
 
 function Player:draw()
     love.graphics.setColor(1, 0, 0)
     love.graphics.circle("fill", self.x, self.y, self.radius)
+    -- love.graphics.circle("fill", self.body:getX(), self.body:getY(), 10)
+    print(self.body:getX() .. ",  " .. self.body:getY())
     love.graphics.setColor(0, 0, 0)
 end
 
 function Player:update(dt, map)
-    self.isColliding = self:checkMapCollision(map)
-    if love.keyboard.isDown("w") and self.isColliding == false then
-        self.y = self.y - 1
+    -- self.isColliding = self:checkMapCollision(map)
+    if love.keyboard.isDown("w") then
+        self.body:applyForce(0, -100)
     end
 
-    if love.keyboard.isDown("s") and self.isColliding == false then
-        self.y = self.y + 1
+    if love.keyboard.isDown("s") then
+        self.body:applyForce(0, 100)
     end
 
-    if love.keyboard.isDown("a") and self.isColliding == false then
-        self.x = self.x - 1
+    if love.keyboard.isDown("a") then
+        self.body:applyForce(-100, 0)
     end
 
-    if love.keyboard.isDown("d") and self.isColliding == false then
-        self.x = self.x + 1
+    if love.keyboard.isDown("d") then
+        self.body:applyForce(100, 0)
     end
+
+    self.x = self.body:getX()
+    self.y = self.body:getY()
 
     -- print(self.x .. ", " .. self.y)
 end
--- main.lua
+++ main.lua
@@ -4,7 +4,8 @@ love.window.setMode(800, 800)
 
 local width, height = love.graphics.getDimensions()
 local score = 0
-local player = Player(width/2, height/2)
+local world = love.physics.newWorld(0, 0, true)
+local player = Player(world, width/2, height/2)
 
 
 local new_map =
@@ -32,9 +33,11 @@ local new_map =
 local map = Map(16, 16, new_map)
 
 function love.load()
+    love.physics.setMeter(64)
 end
 
 function love.update(dt)
+    world:update(dt)
     player:update(dt, map)
     -- map:isColliding(player)
 end
@@ -51,28 +54,5 @@ function love.draw()
     love.graphics.setColor(0, 0, 0)
     love.graphics.print(score, 0, 0)
     love.graphics.setBackgroundColor(1, 1, 1)
-end
-
-function love.mousepressed(x, y, button, istouch, presses)
-    if button == 1 then
-        local mouseToTarget = distanceToTarget(x, y, target.x, target.y)
-        if mouseToTarget < target.radius then
-            score = score + 1
-        end
-    end
-end
-
-function distanceToTarget(mouseX, mouseY, targetX, targetY)
-    return math.sqrt((targetX - mouseX)^2 + (targetY - mouseY) ^ 2)
-end
-
--- Is the player leaving the window?
-function isPlayerColliding(x, y)
-
-    if x >= width or x <= 0 or y >= height or y <= 0 then
-        return true
-    else
-        return false
-    end
 end
 
